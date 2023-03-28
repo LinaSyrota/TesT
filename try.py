4f019aa9c72522277133f0cacd73470f650cdb2c
LinaSyrota <angelinasirotaalex@gmail.com>
2023-03-28 11:54:35 +0300

    more

--- try.py
+++ try.py
@@ -1,7 +1,18 @@
 import re
-with open('test_file.txt', 'r') as file:
-    text = file.read()
+
+#file = open('test_file.txt').read()
+ 
+def words(text):
+    # text = file.read()
     words = re.split('[,:; ]+', text)
     num_words = len(words)
     print(num_words)
 
+def sentence(text):
+    sentences = re.split('[.!?]+', text)
+    num_s = len(sentences)
+    print(num_s)
+    
+file = open('test_file.txt').read()
+words(file)
+sentence(file)
